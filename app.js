8cd95cc05ae3d67800e815f07ff4390c6779fd3b
bishaldsrija08 <bishaldsrijal1@gmail.com>
2025-05-18 09:30:46 +0545

    Express, Port Numbers, Github basics

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("hello")
+const express = require("express")
+const app = express()
+
+app.get("/", (req, res)=>{
+    res.send("<h1>This is Homepage.</h1>")
+})
+
+app.listen(3000, ()=>{
+    console.log("Project has started at port 3000")
+})
