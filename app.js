43beb8d7e3e43ac84ef4c66214015800199bb7e1
bishaldsrija08 <bishaldsrijal1@gmail.com>
2025-05-21 23:34:55 +0545

    login and register page

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const express = require("express")
 const app = express()
 
+//Set view engine 
+app.set('view engine', 'ejs')
+
 app.get("/", (req, res)=>{
-    res.send("<h1>This is Homepage.</h1>")
+res.render('home') //second argument in object
 })
+
+app.get("/register", (req,res)=>{
+    res.render("auth/register.ejs")
+})
+
+app.get("/login", (req,res)=>{
+    res.render("auth/login")
+})
+
+app.use(express.static('public/css/'))
 
 app.listen(3000, ()=>{
     console.log("Project has started at port 3000")
